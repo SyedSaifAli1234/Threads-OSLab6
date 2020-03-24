d59ee492cc28fdbc937f6862d83ecbe1414da92c
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-03-25 04:17:57 +0500

    ignored

--- Q1_164082.c
+++ Q1_164082.c
@@ -1,30 +1,42 @@
-#include<stdio.h>
-
-#include<pthread.h>
-
-int arr[50], n, i;
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
 
 
+int arr[50];
+int n = 0;
+int i = 0;
 
-void *average(){
+void *average(void* param){
 
-	int sum = 0;
-	float avg = 0;
 
-	printf("Input number ");
-	scanf("%d", &n);
+	char a[10] = *(char*) param;
+ 	char *new_ptr = new int; 
+ 	*new_ptr = a;
+ 	printf("%s\n", *new_ptr);
+	// int *new_ptr=new int; 
+	// *new_ptr=a;
+	// (*new_ptr)++;
 
-	for(i = 0; i < n; i++){
-		scanf("%d", &arr[i]);
-	}
+ // 	pthread_exit( (void*) new_ptr);
 
-	for(i = 0; i<n; i++){
-		sum=sum+arr[i];
-	}
+	// int sum = 0;
+	// float avg = 0;
 
-	avg = sum/n;
+	// printf("Input number ");
+	// scanf("%d", &n);
 
-	printf("The average  is : %f",avg);
+	// for(i = 0; i < n; i++){
+	// 	scanf("%d", &arr[i]);
+	// }
+
+	// for(i = 0; i<n; i++){
+	// 	sum=sum+arr[i];
+	// }
+
+	// avg = sum/n;
+
+	// printf("The average  is : %f",avg);
 }
 
 void *maximum(){
@@ -69,10 +81,10 @@ pthread_t t3;
 	n = pthread_create(&t1, NULL, &average, &argv);
 	pthread_join(t1, NULL);
 
-	n = pthread_create(&t2, NULL, &Minimum, NULL);
-	pthread_join(t2, NULL);
+	//n = pthread_create(&t2, NULL, &Minimum, NULL);
+	//pthread_join(t2, NULL);
 
-    n = pthread_create(&t3, NULL, &maximum, NULL);
-	pthread_join(t3, NULL);
+    //n = pthread_create(&t3, NULL, &maximum, NULL);
+	//pthread_join(t3, NULL);
 
 }
