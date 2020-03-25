73369172eb8ef33daabdf5cc158264791f7224eb
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-03-25 18:06:41 +0500

    Update Q2_164082.cpp

--- Q2_164082.cpp
+++ Q2_164082.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-using namespace std;
 #include <string.h>
+#include <cstring>
+#include <unistd.h>
+using namespace std;
 
 struct Args{
 
@@ -10,19 +12,21 @@ struct Args{
 
 void* Fibonacci(void* param){
   int num = 0;
+  cout<<"In thread"<<endl;
   Args *test =(Args*) param;
 
-  for(int i=1; i<test->length; i++){
+  cout<<"Length = "<<test->number;
+  cout<<endl;
+  // for(int i=1; i<test->length; i++){
 
-       avg = avg + atoi(test->array[i]);
+  //      avg = avg + atoi(test->array[i]);
 
-  }
+  // }
 
-  test->length = test->length-1;
-  avg = avg/test->length;
+  // test->length = test->length-1;
+  // avg = avg/test->length;
 
-  test->length = avg;
-
+  // test->length = avg;
   pthread_exit( (void*) test);
   // or u can use: return (void*) updatedStudent; 
   //do not use exit routine, it will terminate the whole process
@@ -44,25 +48,30 @@ int main(int argc, char** argv){
 	int j = 1;
 
 	for(int i=0; i< argc-1; i++){											//Assigning values
-		argue[i].array = *argv[j];
+		argue[i].number = (*argv[j]) - '0';
+		//cout<<"argv["<<i<<"] = "<<*argv[j]<<endl;
+		cout<<"argue["<<i<<"].number = "<<argue[i].number<<endl;
 		j++;
 	}
 
 
 	for(int i=0; i< argc-1; i++){
+		usleep(1000000);
 		if (pthread_create(&id, NULL, &Fibonacci, &argue[i])==-1){      //For loop to create
 			cout<<"Thread Creation Failed!"<<endl;						//threads.
 	     	return 1;
 	  	}
-
-	  	Args *fib;
-		pthread_join(id, (void**) &fib);
-		
-		cout<<"Fibonacci series by Thread "<<i<<" = "; 
-		for(int i = 0; i < argc; i++){
-			cout<<fib->array[i];
-		}
 	}
+
+	Args *fib;
+	pthread_join(id, (void**) &fib);
+		
+	// 	cout<<"Fibonacci series by Thread "<<i<<" = "; 
+	// 	for(int i = 0; i < argc; i++){
+	// 		cout<<fib->array[i];
+	// 	}
+	// }
+	cout<<"exiting function"<<endl;
 }
 
 
