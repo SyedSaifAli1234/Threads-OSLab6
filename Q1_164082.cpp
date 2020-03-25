eaa3306c4041907a384f62a876a388542a2ad3c9
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-03-25 16:35:41 +0500

    Average done

--- Q1_164082.cpp
+++ Q1_164082.cpp
@@ -9,18 +9,51 @@ struct Args{
 
 };
 
-void* myFunc(void* param){
-
+void* Average(void* param){
+  float avg = 0;
   Args *test =(Args*) param;
 
-  for(int i=1; i<test->length; i++) 
+  for(int i=1; i<test->length; i++){
        cout<<test->array[i]<<endl;
+       avg = avg + atoi(test->array[i]);
+  }
+  test->length = test->length-1;
+  avg = avg/test->length;
+
+  test->length = avg;
 
   pthread_exit( (void*) test);
   // or u can use: return (void*) updatedStudent; 
   //do not use exit routine, it will terminate the whole process
 
 }
+
+
+void *maximum(void* param){
+
+  int max = 0;
+  Args *test =(Args*) param;
+
+  for(int i=1; i<test->length; i++){
+       cout<<test->array[i]<<endl;
+       avg = avg + atoi(test->array[i]);
+  }
+  test->length = test->length-1;
+  avg = avg/test->length;
+
+  test->length = avg;
+
+  pthread_exit( (void*) test);
+
+}
+
+
+
+
+
+
+
+
 
 
 int main(int argc, char** argv){
@@ -31,16 +64,51 @@ int main(int argc, char** argv){
 
   argue.array = argv;
   argue.length = argc;
- 
-  if (pthread_create(&id, NULL, &myFunc, &argue)==-1){
+
+  if (pthread_create(&id, NULL, &Average, &argue)==-1){           //Average
 
       cout<<"Thread Creation Failed!"<<endl;
       return 1;
   }
   
 
-  Args *argue2;
-  pthread_join(id, (void**) &argue2);
+  Args *average;
+  pthread_join(id, (void**) &average);
+  cout<<"\nAverage is = "<<average->length;
 
-  cout<<"\nThread exiting\n";
+
+
+
+  if (pthread_create(&id, NULL, &Maximum, &argue)==-1){           //Maximum
+
+      cout<<"Thread Creation Failed!"<<endl;
+      return 1;
+  }
+
+  Args *max;
+  pthread_join(id, (void**) &max);
+  cout<<"\nMaximum is = "<<max->length;
+
+
+
+
+  // if (pthread_create(&id, NULL, &Minimum, &argue)==-1){           //Minimum
+
+  //     cout<<"Thread Creation Failed!"<<endl;
+  //     return 1;
+  // }
+
+  // Args *min;
+  // pthread_join(id, (void**) &min);
+  // cout<<"\nMinimum is = "<<min->length;
+
 }
+
+
+
+
+
+
+
+
+
